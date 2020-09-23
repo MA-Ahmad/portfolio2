89309fcb7fbf6186f0b13d9a1b54442cbe4785ef
MA-Ahmad <muhammad.ahmad8043@gmail.com>
2020-09-23 05:49:31 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -16,20 +16,12 @@ breakpoints.lg = breakpoints[2];
 breakpoints.xl = breakpoints[3];
 
 const newTheme = {
-  ...theme,
-  breakpoints,
-  colors: {
-    ...theme.colors,
-    brand: {
-      900: "#1a365d",
-      800: "#153e75",
-      700: "#2a69ac"
-    }
-  }
+  ...customTheme,
+  breakpoints
 };
 
 ReactDOM.render(
-  <ThemeProvider theme={customTheme}>
+  <ThemeProvider theme={newTheme}>
     <ColorModeProvider>
       <CSSReset />
       <App />
