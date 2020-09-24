6bd57e4e2b8af1674de4b59df20804db16aa2c98
MA-Ahmad <muhammad.ahmad8043@gmail.com>
2020-09-24 05:22:48 -0700

    Aticles routed added

--- src/Routes.js
+++ src/Routes.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import About from "./components/About";
+import Articles from "./components/Articles";
 import OpenSource from "./components/OpenSource";
 import Blog from "./components/Blog";
 import Header from "./header";
@@ -12,6 +13,7 @@ const Routes = () => {
       <Header />
       <Switch>
         <Route exact path="/" component={About} />
+        <Route exact path="/articles" component={Articles} />
         <Route exact path="/open-source" component={OpenSource} />
         <Route exact path="/blog" component={Blog} />
       </Switch>
