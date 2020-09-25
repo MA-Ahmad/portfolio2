f166ba6465db0c6f4e2c2ad55472361a5525c618
MA-Ahmad <muhammad.ahmad8043@gmail.com>
2020-09-25 08:56:55 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Header from "./header";
 import About from "./components/About";
 import OpenSource from "./components/OpenSource";
-import Blog from "./components/Blog";
+import Projects from "./components/Projects";
 import Footer from "./Footer";
 import Layout from "./layouts/layout";
 import Home from "./components/Home";
@@ -16,7 +16,7 @@ function App() {
     //   <Switch>
     //     <Route exact path="/" component={About} />
     //     <Route exact path="/open-source" component={OpenSource} />
-    //     <Route exact path="/blog" component={Blog} />
+    //     <Route exact path="/projects" component={Projects} />
     //   </Switch>
     //   <Footer />
     // </Router>
