b0e89ee6bb43b8d53c5d90e31ad6cf3a0b4202ad
MA-Ahmad <muhammad.ahmad8043@gmail.com>
2020-09-28 11:29:02 -0700

    Updates

---
+++ precache-manifest.36b70761a7d640d8e1985120f928ae04.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03c9b72b70eb40d7f900620cd687a70a",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "c092c07065e8e2df5c8f",
+    "url": "/portfolio/static/js/2.a3e1f753.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/portfolio/static/js/2.a3e1f753.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a738850ff95a85d442ef",
+    "url": "/portfolio/static/js/main.908871c5.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "08dd3b63be85b0dd9cd2d5c2bdc9172c",
+    "url": "/portfolio/static/media/codenya.08dd3b63.png"
+  },
+  {
+    "revision": "69feb01fbcc4fcdda65890a8ce987c97",
+    "url": "/portfolio/static/media/fast.69feb01f.png"
+  },
+  {
+    "revision": "7ec7f937d6f37dd19a2d8559e618b39f",
+    "url": "/portfolio/static/media/gosave.7ec7f937.png"
+  },
+  {
+    "revision": "393f543f7d99f4b9a97ef7609e9afb2a",
+    "url": "/portfolio/static/media/notch.393f543f.png"
+  },
+  {
+    "revision": "fa97bffc7d18fe3fa5a6798b9cf1b375",
+    "url": "/portfolio/static/media/peterpitch.fa97bffc.png"
+  },
+  {
+    "revision": "f1770f91679b30b9dbe7b1b002234f89",
+    "url": "/portfolio/static/media/souq.f1770f91.png"
+  },
+  {
+    "revision": "e8d6b05ec7f66d70448efc352dbf3ba5",
+    "url": "/portfolio/static/media/workof.e8d6b05e.png"
+  }
+]);
