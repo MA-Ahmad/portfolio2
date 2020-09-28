c4d5163421957da3d42936832f527bfe63c8ca6b
MA-Ahmad <muhammad.ahmad8043@gmail.com>
2020-09-28 04:33:42 -0700

    Update Projects.js

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React from 'react';
 import {
   Box,
   Stack,
@@ -10,82 +10,135 @@ import {
   Tooltip,
   IconButton,
   Image,
-  useColorMode
-} from "@chakra-ui/core";
-import { FaGraduationCap } from "react-icons/fa";
-import { FiGithub } from "react-icons/fi";
-import { BsFillBriefcaseFill } from "react-icons/bs";
-import PhaedraLogo from "../assets/images/phaedra.jpeg";
-import BrainxLogo from "../assets/images/brainxtech.png";
-import CodenyaLogo from "../assets/images/codenya.png";
-import BrainplowLogo from "../assets/images/brainplow.jpg";
-import UniLogo from "../assets/images/fast.png";
+  useColorMode,
+  Badge,
+  Grid
+} from '@chakra-ui/core';
+import { FaGraduationCap } from 'react-icons/fa';
+import { FiGithub } from 'react-icons/fi';
+import { BsFillBriefcaseFill } from 'react-icons/bs';
+import PhaedraLogo from '../assets/images/phaedra.jpeg';
+import BrainxLogo from '../assets/images/brainxtech.png';
+import CodenyaLogo from '../assets/images/codenya.png';
+import BrainplowLogo from '../assets/images/brainplow.jpg';
+import peterpitch_img from '../assets/images/Projects/peterpitch.png';
+import workof_img from '../assets/images/Projects/workof.png';
+import notch_img from '../assets/images/Projects/notch.png';
+import souq_img from '../assets/images/Projects/souq.png';
+import gosave_img from '../assets/images/Projects/gosave.png';
 
-function Company({ title, role, skills, logo_url, period, logo, colorMode }) {
+function Project_test({ title, desc, tech, colorMode, image_url }) {
   return (
-    <Box
-      paddingX={4}
-      paddingY={5}
-      _hover={{ shadow: "md" }}
-      // cursor="pointer"
-      // borderWidth="1px"
-      borderColor={`mode.${colorMode}.border`}
-      backgroundColor={`mode.${colorMode}.cardBG`}
-      position="relative"
-      rounded="md"
-    >
-      {/* <Box
-        as={FiBookmark}
-        size="25px"
-        width="1.5rem"
-        height="1.5rem"
-        position="absolute"
-        color="#cbd5e0"
-        fill={`mode.${colorMode}.background`}
-        right="0.5rem"
-        top="-8px"
-        marginBottom="1rem"
-      /> */}
-      <Flex justifyContent="space-between">
-        <Flex>
-          <Image
-            rounded="full"
-            htmlWidth="50px"
-            objectFit="cover"
-            src={logo}
-            alt="Brainx technologies"
-          />
-          <Stack spacing={2} pl={3}>
-            <Heading fontSize="xl" color={`mode.${colorMode}.career.text`}>
-              {title}
-            </Heading>
-            <Heading fontSize="sm" color={`mode.${colorMode}.career.subtext`}>
-              {role}
-            </Heading>
-          </Stack>
-        </Flex>
-        <Stack>
-          <Text fontSize={14} color={`mode.${colorMode}.career.subtext`}>
-            {period}
+    <Stack marginBottom={5}>
+      <Heading as="h4" size="md" color={`mode.${colorMode}.career.text`}>
+        <Flex alignItems="center">
+          <Text as="span" color={`mode.${colorMode}.career.text`}>
+            {title}
           </Text>
+        </Flex>
+      </Heading>
+      <Flex
+        p={5}
+        borderWidth="1px"
+        borderColor={`mode.${colorMode}.border`}
+        backgroundColor={`mode.${colorMode}.cardBG`}
+        position="relative"
+        rounded="md"
+      >
+        <Box>
+          <Image
+            rounded="md"
+            width="12em"
+            // src={image_url ? image_url : 'https://bit.ly/2k1H1t6'}
+            src={workof_img}
+          />
+        </Box>
+        <Stack mt={2} pl={4}>
+          <Text
+            fontSize="lg"
+            fontWeight="semibold"
+            lineHeight="short"
+            color={`mode.${colorMode}.subtext`}
+          >
+            {desc}
+          </Text>
+          <Stack isInline>
+            {tech.split(',').map(tag => (
+              <Tag
+                size="sm"
+                padding="0 3px"
+                key={tag}
+                color="#4299E1"
+                fontWeight="bold"
+                color={`mode.${colorMode}.career.subtext`}
+                bgColor={`mode.${colorMode}.career.tagBG`}
+              >
+                {tag}
+              </Tag>
+            ))}
+          </Stack>
         </Stack>
       </Flex>
-      <Stack spacing={2} mt={3} isInline alignItems="center">
-        {skills.split(",").map(skill => (
-          <Tag
-            size="sm"
-            padding="0 3px"
-            key={skill}
-            color="#4299E1"
-            fontWeight="bold"
-            color={`mode.${colorMode}.career.subtext`}
-            bgColor={`mode.${colorMode}.career.tagBG`}
-          >
-            {skill}
-          </Tag>
-        ))}
-      </Stack>
-    </Box>
+    </Stack>
+  );
+}
+
+function Project({ title, desc, tech, colorMode, image_url }) {
+  return (
+    <Stack marginBottom={5}>
+      <Heading as="h4" size="md" color={`mode.${colorMode}.career.text`}>
+        <Flex alignItems="center">
+          <Text as="span" color={`mode.${colorMode}.career.text`}>
+            {title}
+          </Text>
+        </Flex>
+      </Heading>
+      <Grid
+        templateColumns="repeat(2, 1fr)"
+        gap={2}
+        p={5}
+        _hover={{ shadow: 'md' }}
+        borderWidth="1px"
+        borderColor={`mode.${colorMode}.border`}
+        backgroundColor={`mode.${colorMode}.cardBG`}
+        position="relative"
+        rounded="md"
+      >
+        <Box w="100%" h="100%">
+          <Image
+            rounded="md"
+            src={image_url ? image_url : 'https://bit.ly/2k1H1t6'}
+          />
+        </Box>
+        <Box w="100%">
+          <Stack mt={2} pl={4}>
+            <Text
+              fontSize="lg"
+              fontWeight="semibold"
+              lineHeight="short"
+              color={`mode.${colorMode}.subtext`}
+            >
+              {desc}
+            </Text>
+            <Stack isInline>
+              {tech.split(',').map(tag => (
+                <Tag
+                  size="sm"
+                  padding="0 3px"
+                  key={tag}
+                  color="#4299E1"
+                  fontWeight="bold"
+                  color={`mode.${colorMode}.career.subtext`}
+                  bgColor={`mode.${colorMode}.career.tagBG`}
+                >
+                  {tag}
+                </Tag>
+              ))}
+            </Stack>
+          </Stack>
+        </Box>
+      </Grid>
+    </Stack>
   );
 }
 
@@ -102,85 +155,44 @@ const Projects = () => {
       maxWidth="48rem"
       paddingTop="3rem"
     >
-      <Stack spacing={2} marginBottom={8}>
-        <Heading color={`mode.${colorMode}.text`}>Hi, I'm Ahmad</Heading>
-        <Text color={`mode.${colorMode}.text`}>
-          I am a Full Stack developer, an open source lover and a writer from
-          Pakistan.
-        </Text>
-        <Text color={`mode.${colorMode}.text`}>
-          This is my digital garden, where I write about the things I'm working
-          on and share what I've learned.
-        </Text>
-      </Stack>
-      <Stack spacing={4} marginBottom={6}>
-        <Heading color={`mode.${colorMode}.career.text`}>
-          <Flex alignItems="center">
-            <Text as="span" color={`mode.${colorMode}.career.text`}>
-              Career
-            </Text>
-            <Stack pl={3}>
-              <Box
-                as={BsFillBriefcaseFill}
-                size="25px"
-                color={`mode.${colorMode}.career.subtext`}
-              />
-            </Stack>
-          </Flex>
-        </Heading>
-        <Company
-          title="Codenya Studio"
-          role="Co-founder, CTO"
-          skills="Project Management, Web Development, Devops"
-          period="2017 - Present"
-          logo={CodenyaLogo}
+      <Stack spacing={2}>
+        <Project
+          title="Peterpitch"
+          desc="Peterpitch is a tool that takes the fun in your company seriously
+          and turns your meetings into real performance boosters. With Peter
+          Pitch, you round up the pack of employees and turn even the shakiest
+          puppies into true wolves."
+          tech="ruby, rails, react, javascript"
+          image_url={peterpitch_img}
           colorMode={colorMode}
         />
-        <Company
-          title="Phaedra Solutions"
-          role="Full Stack Developer"
-          skills="Ruby, Ruby on Rails, Javascript, React, Devops"
-          period="2019 - Present"
-          logo={PhaedraLogo}
+        <Project
+          title="Notch"
+          desc="This is a project management website for design and procurement teams. It helps to collaborate and manage all of your floor plans in one place, writing specs, budgeting and timelines."
+          tech="ruby, rails, javascript"
+          image_url={notch_img}
           colorMode={colorMode}
         />
-        <Company
-          title="Brainx Technologies"
-          role="Software Engineer"
-          skills="Ruby, Ruby on Rails, Javascript, Python"
-          period="2018 - 2019"
-          logo={BrainxLogo}
+        <Project
+          title="Workof Ecommerce"
+          desc="This is an Ecommerce furniture site. It provides a single platform to multiple vendors where they can put their products on sale."
+          tech="ruby, rails, javascript"
+          image_url={workof_img}
           colorMode={colorMode}
         />
-        <Company
-          title="Brainplow"
-          role="Software Engineer"
-          skills="Javascript, python, Angular"
-          period="2017 - 2018"
-          logo={BrainplowLogo}
+        <Project
+          title="Souq of Qatar"
+          desc="Souq of Qatar is online comparison website. This website is independent of any retailers, it displays the products of all the major stores.
+          Just like when you compare prices and products while visiting physical shops and stores, online price comparison behaves in the same way."
+          tech="ruby, rails"
+          image_url={souq_img}
           colorMode={colorMode}
         />
-      </Stack>
-      <Stack spacing={4}>
-        <Heading>
-          <Flex alignItems="center">
-            <Text as="span" color={`mode.${colorMode}.career.text`}>
-              Education
-            </Text>
-            <Stack pl={2}>
-              <Box
-                as={FaGraduationCap}
-                color={`mode.${colorMode}.career.subtext`}
-              />
-            </Stack>
-          </Flex>
-        </Heading>
-        <Company
-          title="National University of Computer and Emerging Sciences"
-          role="Bachelor's Degree in Computer Science"
-          skills="Project Management, Web Development, Algorithms, Data Structures"
-          period="2013 - 2017"
-          logo={UniLogo}
+        <Project
+          title="GoSave"
+          desc="The GoSave app let's you send allowances, assign chores, and approve spending all from your smartphone. These actions appear on each kid's toy, along with a live bank balance so they can watch their savings grow."
+          tech="rails"
+          image_url={gosave_img}
           colorMode={colorMode}
         />
       </Stack>
