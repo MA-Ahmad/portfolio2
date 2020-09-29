a6b2a1daaafc4d89a06e45f53e0e602178e845a5
MA-Ahmad <muhammad.ahmad8043@gmail.com>
2020-09-29 01:45:47 -0700

    removed unused and commented code

--- src/App.js
+++ src/App.js
@@ -1,26 +1,8 @@
-import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Header from "./header";
-import About from "./components/About";
-import OpenSource from "./components/OpenSource";
-import Projects from "./components/Projects";
-import Footer from "./Footer";
-import Layout from "./layouts/layout";
-import Home from "./components/Home";
+import React from 'react';
+import Layout from './layouts/layout';
 
 function App() {
-  return (
-    <Layout />
-    // <Router>
-    //   <Header />
-    //   <Switch>
-    //     <Route exact path="/" component={About} />
-    //     <Route exact path="/open-source" component={OpenSource} />
-    //     <Route exact path="/projects" component={Projects} />
-    //   </Switch>
-    //   <Footer />
-    // </Router>
-  );
+  return <Layout />;
 }
 
 export default App;
