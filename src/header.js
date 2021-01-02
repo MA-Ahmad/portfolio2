70d0cfb6e117782a6680c2b75d04099502173da6
MA-Ahmad <muhammad.ahmad8043@gmail.com>
2021-01-02 12:29:53 -0800

    updated header.js

--- src/header.js
+++ src/header.js
@@ -65,15 +65,6 @@ const Header = props => {
         </Heading>
 
         <Box display={{ sm: 'block', md: 'none' }} onClick={handleToggle}>
-          {/* <svg
-            fill="black"
-            width="12px"
-            viewBox="0 0 20 20"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <title>Menu</title>
-            <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
-          </svg> */}
           <FiMenu
             fontSize="2.5rem"
             color={colorMode === 'light' ? '#000' : '#fff'}
