23a85d70d7cbc2c773406603bca0bca04e2d5e91
MA-Ahmad <muhammad.ahmad8043@gmail.com>
2021-03-22 01:21:45 -0700

    Update OpenSource.js

--- src/components/OpenSource.js
+++ src/components/OpenSource.js
@@ -139,7 +139,7 @@ const OpenSource = () => {
       languages: 'Javascript, React, ChakraUi, Formik',
       url: 'https://github.com/MA-Ahmad/reactBlog',
       live: 'https://ma-ahmad.github.io/reactBlog',
-      stars: '4',
+      stars: '9',
       fork: '1'
     },
     {
@@ -148,33 +148,37 @@ const OpenSource = () => {
       languages: 'Javascript, ChakraUi',
       url: 'https://github.com/MA-Ahmad/blog-app-react-frontend',
       live: 'https://blog-frontend-react.herokuapp.com',
-      stars: '3'
+      stars: '6'
     },
     {
       title: 'MA-Ahmad/react_rails_blog',
       description: 'A simple react+rails(RR) CRUD app with tailwindcss.',
       languages: 'Ruby, Rails, Javascript, React, TailwindCss',
       url: 'https://github.com/MA-Ahmad/react_rails_blog',
-      live: 'https://react-on-rails-blog.herokuapp.com'
+      live: 'https://react-on-rails-blog.herokuapp.com',
+      stars: '2'
     },
     {
       title: 'MA-Ahmad/blog-app-rails-backend',
       description: 'Rails backend api for blog app with react on frontend.',
       languages: 'Ruby, Rails',
-      url: 'https://github.com/MA-Ahmad/blog-app-rails-backend'
+      url: 'https://github.com/MA-Ahmad/blog-app-rails-backend',
+      stars: '2'
     },
     {
       title: 'MA-Ahmad/rails-authentication-app',
       description:
         'Simple Authentication API that can be used with any front-end app like React, Vue, Angular app.',
       languages: 'Ruby, Rails',
-      url: 'https://github.com/MA-Ahmad/rails-authentication-app'
+      url: 'https://github.com/MA-Ahmad/rails-authentication-app',
+      stars: '1'
     },
     {
       title: 'MA-Ahmad/Video-to-text',
       description: 'Convert a video to text using ruby.',
       languages: 'Ruby',
-      url: 'https://github.com/MA-Ahmad/Video-to-text'
+      url: 'https://github.com/MA-Ahmad/Video-to-text',
+      stars: '1'
     }
   ];
   return (
