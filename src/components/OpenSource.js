0a42735760bdcb1d6785eb750a2387822977d695
MA-Ahmad <muhammad.ahmad8043@gmail.com>
2021-03-22 01:32:13 -0700

    created a file for respositories list

--- src/components/OpenSource.js
+++ src/components/OpenSource.js
@@ -14,6 +14,7 @@ import { AiOutlineStar, AiOutlineShareAlt } from 'react-icons/ai';
 import { FiGithub } from 'react-icons/fi';
 import { BiBookBookmark } from 'react-icons/bi';
 import { HiOutlineExternalLink } from 'react-icons/hi';
+import { repos } from '../data/repositories';
 
 function Repo({
   title,
@@ -131,56 +132,7 @@ function Repo({
 
 const OpenSource = () => {
   const { colorMode } = useColorMode();
-  const repos = [
-    {
-      title: 'MA-Ahmad/reactBlog',
-      description:
-        'A React Blog App built by using libraries like ChakraUI, Formik.',
-      languages: 'Javascript, React, ChakraUi, Formik',
-      url: 'https://github.com/MA-Ahmad/reactBlog',
-      live: 'https://ma-ahmad.github.io/reactBlog',
-      stars: '9',
-      fork: '1'
-    },
-    {
-      title: 'MA-Ahmad/blog-app-react-frontend',
-      description: 'React frontend of a blog app with rails on backend.',
-      languages: 'Javascript, ChakraUi',
-      url: 'https://github.com/MA-Ahmad/blog-app-react-frontend',
-      live: 'https://blog-frontend-react.herokuapp.com',
-      stars: '6'
-    },
-    {
-      title: 'MA-Ahmad/react_rails_blog',
-      description: 'A simple react+rails(RR) CRUD app with tailwindcss.',
-      languages: 'Ruby, Rails, Javascript, React, TailwindCss',
-      url: 'https://github.com/MA-Ahmad/react_rails_blog',
-      live: 'https://react-on-rails-blog.herokuapp.com',
-      stars: '2'
-    },
-    {
-      title: 'MA-Ahmad/blog-app-rails-backend',
-      description: 'Rails backend api for blog app with react on frontend.',
-      languages: 'Ruby, Rails',
-      url: 'https://github.com/MA-Ahmad/blog-app-rails-backend',
-      stars: '2'
-    },
-    {
-      title: 'MA-Ahmad/rails-authentication-app',
-      description:
-        'Simple Authentication API that can be used with any front-end app like React, Vue, Angular app.',
-      languages: 'Ruby, Rails',
-      url: 'https://github.com/MA-Ahmad/rails-authentication-app',
-      stars: '1'
-    },
-    {
-      title: 'MA-Ahmad/Video-to-text',
-      description: 'Convert a video to text using ruby.',
-      languages: 'Ruby',
-      url: 'https://github.com/MA-Ahmad/Video-to-text',
-      stars: '1'
-    }
-  ];
+
   return (
     <Stack
       as="main"
---
+++ src/data/repositories.js
@@ -0,0 +1,57 @@
+export const repos = [
+  {
+    title: 'MA-Ahmad/reactBlog',
+    description:
+      'A React Blog App built by using libraries like ChakraUI, Formik.',
+    languages: 'Javascript, React, ChakraUi, Formik',
+    url: 'https://github.com/MA-Ahmad/reactBlog',
+    live: 'https://ma-ahmad.github.io/reactBlog',
+    stars: '9',
+    fork: '1'
+  },
+  {
+    title: 'MA-Ahmad/blog-app-react-frontend',
+    description: 'React frontend of a blog app with rails on backend.',
+    languages: 'Javascript, ChakraUi',
+    url: 'https://github.com/MA-Ahmad/blog-app-react-frontend',
+    live: 'https://blog-frontend-react.herokuapp.com',
+    stars: '6'
+  },
+  {
+    title: 'MA-Ahmad/notebook',
+    description: 'Make notes for your daily work.',
+    languages: 'Typescript, ChakraUi',
+    url: 'https://github.com/MA-Ahmad/notebook',
+    stars: '2'
+  },
+  {
+    title: 'MA-Ahmad/react_rails_blog',
+    description: 'A simple react+rails(RR) CRUD app with tailwindcss.',
+    languages: 'Ruby, Rails, Javascript, React, TailwindCss',
+    url: 'https://github.com/MA-Ahmad/react_rails_blog',
+    live: 'https://react-on-rails-blog.herokuapp.com',
+    stars: '2'
+  },
+  {
+    title: 'MA-Ahmad/blog-app-rails-backend',
+    description: 'Rails backend api for blog app with react on frontend.',
+    languages: 'Ruby, Rails',
+    url: 'https://github.com/MA-Ahmad/blog-app-rails-backend',
+    stars: '2'
+  },
+  {
+    title: 'MA-Ahmad/rails-authentication-app',
+    description:
+      'Simple Authentication API that can be used with any front-end app like React, Vue, Angular app.',
+    languages: 'Ruby, Rails',
+    url: 'https://github.com/MA-Ahmad/rails-authentication-app',
+    stars: '1'
+  },
+  {
+    title: 'MA-Ahmad/Video-to-text',
+    description: 'Convert a video to text using ruby.',
+    languages: 'Ruby',
+    url: 'https://github.com/MA-Ahmad/Video-to-text',
+    stars: '1'
+  }
+];
