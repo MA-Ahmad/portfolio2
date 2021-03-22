29ac0400d528931b9f16bededc7f9128d159cdcb
MA-Ahmad <muhammad.ahmad8043@gmail.com>
2021-03-22 03:26:35 -0700

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.a975aedcffc2d143a11adfc810bab36c.js"
+  "/precache-manifest.6f70c5676e792b2e648d71fbdb4d6683.js"
 );
 
 self.addEventListener('message', (event) => {
