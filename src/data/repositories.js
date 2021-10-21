475ec7dcbc627fd43d0ba46e6f93d3e0ef6ba606
MA-Ahmad <muhammad.ahmad8043@gmail.com>
2021-10-21 11:02:54 -0700

    update repositories

--- src/data/repositories.js
+++ src/data/repositories.js
@@ -16,7 +16,7 @@ export const repos = [
     languages: 'Nextjs, chakraUi',
     url: 'https://github.com/MA-Ahmad/myPortfolio',
     live: 'https://my-portfolio-ma-ahmad.vercel.app/',
-    stars: '56',
+    stars: '57',
     fork: '13'
   },
   {
